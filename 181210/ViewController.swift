9212edd4cb077d211f6c8cf83c2cc41d5e9f418b
Eevee <dumpling0522@gmail.com>
2019-11-28 20:57:39 +0800

    update

--- 181210/ViewController.swift
+++ 181210/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    @IBOutlet weak var ultimateInfestationImageView: UIImageView!
+    @IBOutlet weak var druidDKImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func distanceValueChange(_ sender: UISlider) {
+        ultimateInfestationImageView.frame.origin.x = CGFloat(39 + 200 * sender.value)
+        druidDKImageView.frame.origin.x = CGFloat(528 - 200 * sender.value)
+        numberLabel.text = String(format: "%.2f", sender.value)
+    }
+    
+    
 }
 
